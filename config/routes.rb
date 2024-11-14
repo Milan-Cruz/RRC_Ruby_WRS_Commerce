2c347bda83d24d540c1c9dcc39d4ed0c9f3f64f8
Milan Cruz <milanmoreira@gmail.com>
2024-11-14 04:28:57 -0600

    Add HomeController and set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get "home/index"
   # Active Admin routes
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   # Devise routes for users with sign_out_via specified
   devise_for :users, controllers: {
-            sessions: "devise/sessions",
-          }, sign_out_via: [:delete, :get]
+                       sessions: "devise/sessions",
+                     }, sign_out_via: [:delete, :get]
+
+  get "profile", to: "users#show", as: :user_profile
 
   # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
@@ -16,5 +19,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Define the root path route ("/")
-  # root "home#index"
+  root "home#index"
 end

0712e386627e594edc1baf6d6b3e0fa21bc3907a
Milan Cruz <milanmoreira@gmail.com>
2024-11-21 03:11:46 -0600

    footer, header and categories partials created

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,7 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"

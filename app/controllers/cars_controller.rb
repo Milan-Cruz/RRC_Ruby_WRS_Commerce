473c3cf81894513cbf5bec88dfe6359ca6217c59
Milan Cruz <milanmoreira@gmail.com>
2024-11-21 00:54:57 -0600

    Update SCSS for section-specific and card styles

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,4 +7,8 @@ class CarsController < ApplicationController
     @query = params[:query]
     @cars = Car.where("make LIKE ? OR model LIKE ?", "%#{@query}%", "%#{@query}%")
   end
+
+  def show
+    @car = Car.find(params[:id])
+  end
 end

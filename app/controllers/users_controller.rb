c1567b0db1e58e93c13bb9fd79499b4b324b5bc7
Milan Cruz <milanmoreira@gmail.com>
2024-12-02 08:39:20 -0600

    resolved edit profile province error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :address, :city, :province, :postal_code)
+    params.require(:user).permit(:name, :email, :address, :city, :province_id, :postal_code)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Associations
-  belongs_to :province
+  belongs_to :province, optional: true
   has_many :searches
   has_many :orders
 

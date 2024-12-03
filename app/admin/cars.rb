ecf84f5d010544ff81f0ae6e9075e80010c03c79
Milan Cruz <milanmoreira@gmail.com>
2024-12-03 10:35:06 -0600

    Update views to display uploaded car images

--- app/admin/cars.rb
+++ app/admin/cars.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Car do
-  permit_params :make, :model, :trim, :year, :mileage, :price, :features, category_ids: []
+  permit_params :make, :model, :trim, :year, :mileage, :price, :features, :image, category_ids: []
 
   index do
     selectable_column
@@ -10,11 +10,18 @@ ActiveAdmin.register Car do
     column :year
     column :mileage
     column :price
+    column "Image" do |car|
+      if car.image.attached?
+        image_tag car.image.variant(resize_to_limit: [50, 50])
+      else
+        "No Image"
+      end
+    end
     actions
   end
 
   form do |f|
-    f.inputs do
+    f.inputs "Car Details" do
       f.input :make
       f.input :model
       f.input :trim
@@ -23,8 +30,32 @@ ActiveAdmin.register Car do
       f.input :price
       f.input :features
       f.input :categories, as: :check_boxes
+      f.input :image, as: :file, hint: f.object.image.attached? ? image_tag(f.object.image.variant(resize_to_limit: [100, 100])) : "No image uploaded"
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :make
+      row :model
+      row :trim
+      row :year
+      row :mileage
+      row :price
+      row :features
+      row :categories do |car|
+        car.categories.map(&:name).join(", ")
+      end
+      row :image do |car|
+        if car.image.attached?
+          image_tag car.image.variant(resize_to_limit: [300, 200])
+        else
+          "No Image"
+        end
+      end
+    end
+    active_admin_comments
   end
 
   filter :make
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,7 @@ class Car < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :price_histories
   has_many :order_items
+  has_one_attached :image
 
   # Validations
   validates :make, :model, :trim, :year, :mileage, :price, presence: true

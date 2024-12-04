269ad2ebe46473122ea7a52184268bd2d72eaf5d
Milan Cruz <milanmoreira@gmail.com>
2024-12-03 23:44:32 -0600

    About and contact models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_02_135236) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_04_053245) do
+  create_table "abouts", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -84,6 +90,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_02_135236) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "address"
+    t.string "email"
+    t.string "phone"
+    t.text "opening_hours"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
